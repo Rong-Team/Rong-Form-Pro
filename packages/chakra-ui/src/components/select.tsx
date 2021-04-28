9a4c007ede489b975767eb536c62ba98f53cbb10
TingzhouJia <joe.tzjia@gmail.com>
2021-04-27 20:57:27 -0400

    revise select

--- packages/chakra-ui/src/components/select.tsx
+++ packages/chakra-ui/src/components/select.tsx
@@ -1,22 +1,22 @@
 import { Select } from "@chakra-ui/react"
+import { ComponentBaseProps } from "../interface"
 
 
-export interface SelectProps {
+export interface SelectProps extends ComponentBaseProps {
     placeholder?:string
     size?:'sm'|'md'|'lg'
     varient?:'outline'|'filled'|'flushed'|'unstyled'
     name:string 
     valueSet:{[name:string]:string}
     multiple?:boolean
-    defaultValues:string|string[]
-    disabled?:boolean,
-    readonly?:boolean
+    defaultValue:string|string[]
+   
 }
 
 
-const SelectComp :React.FC<SelectProps>=({placeholder,size,varient,name,valueSet,disabled,readonly})=>{
+const SelectComp :React.FC<SelectProps>=({valueSet,...props})=>{
     return (
-        <Select size={size} variant={varient} name={name} placeholder={placeholder} isDisabled={disabled} isReadOnly={readonly}>
+        <Select   {...props}>
             {
                 Object.keys(valueSet).map((item,index)=>{
                     return <option value={valueSet[item]} key={name+index}>
