44aadbfd533239bd2a14065635b4ed75de8efd9b
TingzhouJia <joe.tzjia@gmail.com>
2021-04-27 20:58:25 -0400

    add switch

---
+++ packages/chakra-ui/src/components/switch.tsx
@@ -0,0 +1,19 @@
+import { Switch } from "@chakra-ui/react";
+import { ComponentBaseProps } from "../interface";
+
+
+export interface SwitchProps extends ComponentBaseProps {
+    size?:'sm'|'md'|'lg'
+    colorTheme?:string
+    defaultChecked?:boolean
+    name:string
+}
+
+const SwitchComp :React.FC<SwitchProps>=(props)=>{
+
+    return (
+        <Switch {...props} ></Switch>
+    )
+}
+
+export default SwitchComp
