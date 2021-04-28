8b02911877a70f096c9bbc00f6e23577e5f30095
TingzhouJia <joe.tzjia@gmail.com>
2021-04-27 23:27:08 -0400

    revise pininput

--- packages/chakra-ui/src/components/pinInput.tsx
+++ packages/chakra-ui/src/components/pinInput.tsx
@@ -1,22 +1,20 @@
 import { HStack, PinInput, PinInputField } from "@chakra-ui/react";
 import React from "react";
-import { ComponentBaseProps } from "../interface";
+import { ComponentBaseProps, ComponentUIProps, ComponentUnionProps } from "../interface";
 
 
-export interface PinInputProps extends ComponentBaseProps {
+export interface PinInputProps extends ComponentUnionProps {
     type?: 'alphanumeric' | 'number'
     opt?: boolean
     mask?: boolean
-    size?: 'sm' | 'md' | 'lg'
     defaultValue?: string
-    colorScheme?: string
     length: number
     name: string
 }
 
 const PinInputComp: React.FC<PinInputProps> = ({ name, length, ...props }) => {
     return (<HStack>
-        <PinInput {...props}>
+        <PinInput {...props} >
             {
                 Array(length).map((each, index) => {
                     return (
