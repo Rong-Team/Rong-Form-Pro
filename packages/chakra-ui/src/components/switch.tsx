2951e5125ce211538cde2c483bca21b1a957a7b0
TingzhouJia <joe.tzjia@gmail.com>
2021-04-29 12:42:11 -0400

    switch

--- packages/chakra-ui/src/components/switch.tsx
+++ packages/chakra-ui/src/components/switch.tsx
@@ -1,15 +1,14 @@
 import { Switch } from "@chakra-ui/react";
-import { ComponentBaseProps } from "../interface";
+import { ComponentBaseProps, ComponentUnionProps } from "../interface";
 
 
-export interface SwitchProps extends ComponentBaseProps {
-    size?:'sm'|'md'|'lg'
-    colorTheme?:string
-    defaultChecked?:boolean
-    name:string
+export interface SwitchProps extends ComponentUnionProps {
+
+    defaultChecked?: boolean
+    name: string
 }
 
-const SwitchComp :React.FC<SwitchProps>=(props)=>{
+const SwitchComp: React.FC<SwitchProps> = (props) => {
 
     return (
         <Switch  {...props} ></Switch>
