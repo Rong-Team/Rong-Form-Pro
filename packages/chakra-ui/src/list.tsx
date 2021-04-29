e4c516e83c79223d8eef4424e61edc9d31937c49
TingzhouJia <joe.tzjia@gmail.com>
2021-04-29 12:43:11 -0400

    revise render list

--- packages/chakra-ui/src/list.tsx
+++ packages/chakra-ui/src/list.tsx
@@ -1,45 +1,53 @@
-import { Stack } from "@chakra-ui/react";
+import { Button, CloseButton, HStack, Stack } from "@chakra-ui/react";
 import React from "react";
 import { ListForm } from "rong-form";
 import { SchemaField } from "./interface";
 import RenderChild from "./item";
-
-export interface ListProps {
-    value: { [name: string]: SchemaField }
-    name: string
-    defaultValue: any
-    displayType?: "column" | 'row'
-}
-
-const RenderList: React.FC<ListProps> = ({ value, name, defaultValue,  }) => {
+import {MinusIcon,AddIcon} from '@chakra-ui/icons'
+import { warning } from "./components/utils";
+import { useUI } from "./context";
 
 
 
-    return <ListForm name={name}>
+const RenderList: React.FC<SchemaField> = ({ listChildren , name, defaultValue,...props }) => {
+
+    const listInfo=props?.componentprops?.list
+    const uistyle=useUI()
+    if(listChildren){
+        return <ListForm name={name}>
         {
             (fields, { add, remove }) => {
                 return (
-                    fields.map((field, index) => {
-                        return <Stack direction="row" flexWrap="wrap" spacing={10}>
-                            {
-                                Object.keys(value).map((item, index2) => {
-                                    const curValue = value[item]
-                                    if (Array.isArray(curValue.name)) {
-                                        return <React.Fragment key={name+'-'+index2}>
-                                            {RenderChild({ ...curValue, })}
-                                        </React.Fragment>
+                    <Stack>
+                        {
+                            fields.map((field, index) => {
+                                return <Stack direction="row" flexWrap="wrap" spacing={10} key={name + '-' + index}>
+                                    {
+                                        Object.keys(listChildren).map((item, index2) => {
+                                            const curValue = listChildren[item]
+                                            if(curValue.type==='list'){
+                                                return <></>
+                                            }
+                                            return <React.Fragment key={name + '-' + index2}>
+                                                {RenderChild({ ...curValue, fieldName: [field.name, curValue.name],uistyle:uistyle||curValue?.uistyle })}
+                                            </React.Fragment>
+                                        })
                                     }
-                                    return <React.Fragment key={name+'-'+index2}>
-                                        {RenderChild({ ...curValue, name: [name, curValue.name] })}
-                                    </React.Fragment>
-
-                                })
-                            }
-                        </Stack>
-                    })
+                                    {listInfo&&listInfo.showRemove&&<CloseButton  onClick={()=>remove(index)}/>}
+                                </Stack>
+                            })
+                        }
+                        {
+                            listInfo&&listInfo.showAdd&&<Button onClick={()=>add()} leftIcon={<AddIcon/>}>{listInfo?.addText}</Button>
+                        }
+                    </Stack>
                 )
             }
         }
     </ListForm>
-
+    }
+    warning(false,"No listChildren props provided")
+    return <></>
 }
+
+export default RenderList
