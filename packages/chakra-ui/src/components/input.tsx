4a7913acc4e853e4f419fbaeb369af2827e50c67
TingzhouJia <joe.tzjia@gmail.com>
2021-05-01 16:38:44 -0400

    input

--- packages/chakra-ui/src/components/input.tsx
+++ packages/chakra-ui/src/components/input.tsx
@@ -1,21 +1,55 @@
-import { Input } from "@chakra-ui/react";
-import { ComponentBaseProps,  ComponentUnionProps } from "../interface";
+import { Input, InputGroup, InputLeftAddon, InputLeftElement, InputRightAddon, InputRightElement, others } from "@chakra-ui/react";
+import React from "react";
+import { ComponentBaseProps, ComponentUnionProps } from "../interface";
 import Password from "./password";
+import { mapSize } from "./utils";
 
 
 
-export interface InputProps extends ComponentUnionProps  {
-    type:'color'|'text'|'email'|'password'|'url'|'date'| 'datetime-local'|'week'|'month'|'file'
+export interface InputProps extends ComponentUnionProps {
+    type: 'color' | 'text' | 'email' | 'password' | 'url' | 'date' | 'datetime-local' | 'week' | 'month' | 'file'
+
+    min?: string,
+    max?: string
+
+    accept?: string
+
+
+    addAfter?: string
+    addBefore?: string
+    prefix?: string
+    postfix?: string
 
 }
 
-const InputComp:React.FC<InputProps>=({type,...props})=>{
-    if(type==='password'){
+const InputComp: React.FC<InputProps> = ({ type, ...props }) => {
+    if (type === 'password') {
         return <Password {...props} />
     }
-    return <Input 
-        {...props}
-    />
+    const { size, colorScheme, addBefore, addAfter, prefix, postfix, ...others } = props
+
+    return <InputGroup size={size} colorScheme={colorScheme}>
+        {
+            addBefore && <InputLeftAddon children={addBefore} />
+        }
+        <InputGroup size={size} colorScheme={colorScheme}>
+            {
+                prefix && <InputLeftElement
+
+                    pointerEvents="none"
+                    children={prefix}
+                />
+            }
+            <Input  {...others} size={size} type={type} />
+            {
+                postfix && <InputRightElement
+                    pointerEvents="none"
+                    children={postfix}
+                />
+            }
+        </InputGroup>
+        {addAfter && <InputRightAddon children={addAfter} />}
+    </InputGroup>
 }
 
 export default InputComp
