35147159d30de31057eb278ff1d3355f09058248
TingzhouJia <joe.tzjia@gmail.com>
2021-05-01 16:39:27 -0400

    utils

--- packages/chakra-ui/src/components/utils.ts
+++ packages/chakra-ui/src/components/utils.ts
@@ -3,4 +3,27 @@ export function warning(valid: boolean, message: string) {
       console.error(`Warning: ${message}`);
     }
   }
+
+
+export function mapSize(size:'xs'|'sm'|'md'|'lg'){
+    switch (size) {
+      case "sm":
+        
+        return {
+          padding:'3.2rem'
+        }
+      case 'md':
+        return {
+          padding:'4.5rem'
+        }
+      case 'lg':{
+        return {padding:'5.6rem'}
+      }
+    
+      default:
+        return {
+          padding:'2.4rem'
+        };
+    }
+}
   
