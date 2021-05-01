9fd3d1189f68f718dff9083ca3f61e93dd80a405
TingzhouJia <joe.tzjia@gmail.com>
2021-04-30 23:22:52 -0400

    input component

--- packages/chakra-ui/src/components/input.tsx
+++ packages/chakra-ui/src/components/input.tsx
@@ -1,16 +1,18 @@
 import { Input } from "@chakra-ui/react";
 import { ComponentBaseProps,  ComponentUnionProps } from "../interface";
+import Password from "./password";
 
 
 
 export interface InputProps extends ComponentUnionProps  {
     type:'color'|'text'|'email'|'password'|'url'|'date'| 'datetime-local'|'week'|'month'|'file'
-    placeholder?:string
-    onChange?:any
-    value?:any
+
 }
 
-const InputComp:React.FC<InputProps>=({...props})=>{
+const InputComp:React.FC<InputProps>=({type,...props})=>{
+    if(type==='password'){
+        return <Password {...props} />
+    }
     return <Input 
         {...props}
     />
