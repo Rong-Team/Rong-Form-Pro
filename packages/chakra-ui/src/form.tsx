b42aa130cb9fb8aa6a227456a9dd2d341661e3c2
TingzhouJia <joe.tzjia@gmail.com>
2021-05-02 00:01:22 -0400

    add widget in form

--- packages/chakra-ui/src/form.tsx
+++ packages/chakra-ui/src/form.tsx
@@ -1,43 +1,50 @@
 import { Stack } from "@chakra-ui/react";
+
 import React, { useImperativeHandle, useRef } from "react";
 import { Form } from "rong-form";
 import { IFormInstance } from "rong-form/lib/Form";
-import { UIProvider, WidgetProvider } from "./context";
-import {  IFieldStore, Meta, SchemaType } from "./interface";
+import { RootMapContext, UIProvider, WidgetProvider } from "./context";
+import { IFieldStore, Meta, SchemaType } from "./interface";
 import RenderChild from "./item";
 import RenderList from "./list";
 
 export interface ISchemaForm {
-    schema:SchemaType
-    widget?:{[name:string]:(control:any,meta:Meta,dependencies?:{[name:string]:IFieldStore})=>React.Component}
-    onFinish?:(values:any,errors:any)=>void
+    schema: SchemaType
+    widget?: { [name: string]: (control: any, meta: Meta, dependencies?: { [name: string]: IFieldStore }) => React.Component | React.ReactNode }
+    onFinish?: (values: any, errors: any) => void
 }
 
-const SchemaForm=React.forwardRef<IFormInstance,ISchemaForm>(({ schema,widget },ref) => {
+const SchemaForm = React.forwardRef<IFormInstance, ISchemaForm>(({ schema, widget }, ref) => {
 
 
-    const {name, properties, uistyle}=schema
-    
-    return (<WidgetProvider value={widget||{}}>
+    const { name, properties, uistyle } = schema
+
+    return (
+
         <Form ref={ref}>
+
             <Stack>
+
                 {
                     Object.keys(properties).map((fieldName, index) => {
                         const field = properties[fieldName]
                         if (field.type === 'list') {
                             return <React.Fragment key={name + index}>
-                                {RenderList(field)}
+                                {RenderList({...field,widgets:widget})}
                             </React.Fragment>
                         } else {
                             return <React.Fragment key={name + index}>
-                                {RenderChild({ ...field, fieldName: field.name,uistyle:{...field?.uistyle,...uistyle} })}
+                                {RenderChild({ ...field, fieldName: field.name, uistyle: { ...field?.uistyle, ...uistyle }, widgets: widget })}
                             </React.Fragment>
                         }
                     })
                 }
+
             </Stack>
+
         </Form>
-    </WidgetProvider>)
+    )
+
 }
 )
 export default SchemaForm
