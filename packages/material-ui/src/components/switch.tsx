83f3d5b44ee07dcbd66341d36dab8350354a1009
TingzhouJia <joe.tzjia@gmail.com>
2021-05-04 23:30:53 -0400

    switch

---
+++ packages/material-ui/src/components/switch.tsx
@@ -0,0 +1,14 @@
+import { Switch } from "@material-ui/core";
+import { ComponentUnionProps, UIInterface } from "../interface";
+
+ type UnionType=ComponentUnionProps &UIInterface
+export interface SwitchProps extends UnionType  {
+
+}
+
+
+const SwitchComp:React.FC<SwitchProps> =(props)=>{
+    return <Switch {...props} />
+}
+
+export default SwitchProps
