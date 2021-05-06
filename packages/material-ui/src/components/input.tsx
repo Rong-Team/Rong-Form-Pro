f9831e13b586374f94e8440a336048a97bb04cd3
TingzhouJia <joe.tzjia@gmail.com>
2021-05-05 23:24:23 -0400

    revise input

--- packages/material-ui/src/components/input.tsx
+++ packages/material-ui/src/components/input.tsx
@@ -1,36 +1,67 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { ComponentUnionProps } from '../interface';
+import { ComponentUnionProps, UIInterface } from '../interface';
 import Password from './password';
+import { Input as MaterialInput, FilledInput, OutlinedInput } from '@material-ui/core';
 
 
-
-export interface InputProps extends ComponentUnionProps {
+type InputType = ComponentUnionProps & UIInterface
+export interface InputProps extends InputType {
   type: string
   inputProps?: {
     min?: any
     max?: any
     step?: any
-    accept?:string
-
+    accept?: string
   }
+  prefix?: string
+  postfix?: string
 }
 
-const Input: React.FC<InputProps> = ({ type, inputProps, ...props }) => {
-  
-  if(type==='password'){
-    return <Password {...props}/>
+const Input: React.FC<InputProps> = ({ type, inputProps, variant, ...props }) => {
+
+  if (type === 'password') {
+    return <Password {...props} />
   }
-  return (<TextField
+  if (variant === 'filled') {
+    return (
+      <FilledInput
+
+        type={type}
+
+
+
+        {
+        ...props
+        }
+        inputProps={{
+          ...inputProps
+        }}
+      />
+    )
+  }
+  else if (variant === 'outlined') {
+    return (
+      <OutlinedInput type={type}
+
+
+
+        {
+        ...props
+        }
+        inputProps={{
+          ...inputProps
+        }} />
+    )
+  }
+  return (<Input
 
 
     type={type}
 
-    
-    InputLabelProps={{
-      shrink: true,
-    }}
+
+
     {
     ...props
     }
