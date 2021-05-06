8b4074391362500b72272f4fbdfd82df1b6e7785
TingzhouJia <joe.tzjia@gmail.com>
2021-05-05 22:48:16 -0400

    revise select

--- packages/material-ui/src/components/select.tsx
+++ packages/material-ui/src/components/select.tsx
@@ -1,12 +1,15 @@
 import { MenuItem, Select } from '@material-ui/core';
 import { ComponentUnionProps } from '../interface';
 export interface SelectProps extends ComponentUnionProps{
-    valueSet:{[name:string]:string}
+    valueSet?:{[name:string]:string}
 }
 
 const SelectComp:React.FC<SelectProps>=({
     valueSet,...props
 })=>{
+    if(!valueSet){
+        return <></>
+    }
     return <Select  {...props}>
         {
             Object.keys(valueSet).map((name,index)=>{
