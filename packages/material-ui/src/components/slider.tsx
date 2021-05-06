627375da377af66b6ef54899155fbcafdfa292ce
TingzhouJia <joe.tzjia@gmail.com>
2021-05-05 22:48:31 -0400

    revise slider

--- packages/material-ui/src/components/slider.tsx
+++ packages/material-ui/src/components/slider.tsx
@@ -5,13 +5,12 @@ export interface SlideProps extends ComponentUnionProps {
     min?:number
     max?:number
     step?:number
-    direction?:'up'|'down'|'left'|'right'
     valueLabelDisplay?:"on" | "off" | "auto" 
 }
 
 const SliderComp:React.FC<SlideProps>=({...props})=>{
 
-    return <Slider {...props}  />
+    return <Slider   {...props}  />
 }
 
 export default SliderComp
